98b2b11bdf494ffb591b6563f18aeba31d2012b8
siddhijain <siddhi0811@gmail.com>
2018-05-31 23:26:35 -0500

    Remove unwanted directories

--- product-details-app/src/main/java/com/target/model/ProductDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.target.model;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductDetailsRepository extends MongoRepository<ProductDetails,String>{
-	
-	public ProductDetails findDocumentById(String productId);
-
-}
